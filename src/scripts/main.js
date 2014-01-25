771f1d5fc5b64679594de894a243896b1ada57f3
Matteo Collina <matteo.collina@gmail.com>
2014-01-25 11:39:05 +0100

    Added autorun.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,30 @@
 bespoke.from('article', {
   keys: true,
   touch: true,
-  bullets: 'li, .bullet',
   scale: true,
   hash: true,
   progress: true,
   state: true,
   forms: true,
-  run: true
+  run: true,
+  bullets: true
 });
 
+var dbs = {};
+
 function level(name) {
+  if (dbs[name] && dbs[name].isOpen()) {
+    return dbs[name];
+  }
   var levelup = require('levelup');
-  return levelup(name, { db: require('level-js') });
+  var db = levelup(name, { db: require('level-js') });
+  dbs[name] = db;
+  return db;
 }
 
-levelgraph = require('levelgraph');
+function levelgraph(name) {
+  var levelgraph = require('levelgraph');
+  return levelgraph(level(name));
+}
+
 n3 = require('levelgraph-n3');
